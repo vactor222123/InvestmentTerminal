11ddaa7eaec9c71232a2649fdfee2dadf252f3e4
vactor222123 <vkulavec18@gmail.com>
2026-08-01 00:18:20 +0200

    Enable SQLite type parsing

--- investment_terminal/database/database.py
+++ investment_terminal/database/database.py
@@ -13,9 +13,10 @@ class Database:
 
     def __init__(self):
 
-        self.connection = sqlite3.connect(
-            Settings.DATABASE_PATH
-        )
+self.connection = sqlite3.connect(
+    Settings.DATABASE_PATH,
+    detect_types=sqlite3.PARSE_DECLTYPES,
+)
 
         self.connection.row_factory = sqlite3.Row
 
